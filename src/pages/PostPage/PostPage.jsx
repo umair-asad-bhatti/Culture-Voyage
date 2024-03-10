745546be451f788042954606a7a30d008c30d26d
umair-asad-bhatti <umaircyber6@gmail.com>
2024-03-10 23:02:20 +0500

    fetching posts on homepage

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -1,9 +1,10 @@
+import { useEffect } from "react"
 
 
 export const PostPage = () => {
 
-
+    useEffect(())
     return <div className={'grid lg:grid-cols-2  grid-cols-1 gap-4 dark:text-textPrimary text-textSecondary'}>
-        home page
+        home  fetch
     </div>
 }
